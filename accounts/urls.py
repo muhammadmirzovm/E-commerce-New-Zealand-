831607499f20fd83a0f331b4eb82b6ddca586b3d
mukhammadmirzo <mukhammadmirzo078@gmail.com>
2026-09-08 16:56:08 +0500

    parolni tiklash va o`zgartirish qo`shildi

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
    path("signup/", views.SignUpView.as_view(), name="signup"),
@@ -7,5 +8,31 @@ urlpatterns = [
    path("logout/", views.CustomLogoutView.as_view(), name="logout"),
    path("profile/", views.ProfileDetailView.as_view(), name="profile"),
    path("profile/edit/", views.ProfileUpdateView.as_view(), name="profile_edit"),
+   path("password-reset/",auth_views.PasswordResetView.as_view(
+           template_name="accounts/password_reset_form.html",
+           email_template_name="accounts/password_reset_email.txt",
+           success_url="/accounts/password-reset/done/",
+       ),name="password_reset",),
+   path("password-reset/done/",auth_views.PasswordResetDoneView.as_view(
+           template_name="accounts/password_reset_done.html",
+       ),name="password_reset_done",),
+   path("reset/<uidb64>/<token>/",
+       auth_views.PasswordResetConfirmView.as_view(
+           template_name="accounts/password_reset_confirm.html",
+           success_url="/accounts/reset/done/",),
+       name="password_reset_confirm",),
+   path("reset/done/",auth_views.PasswordResetCompleteView.as_view(
+           template_name="accounts/password_reset_complete.html",),
+       name="password_reset_complete",),
+   path(
+   "password-change/",
+   auth_views.PasswordChangeView.as_view(
+       template_name="accounts/password_change_form.html",
+       success_url="done/",),name="password_change",),
+path(
+   "password-change/done/",
+   auth_views.PasswordChangeDoneView.as_view(
+       template_name="accounts/password_change_done.html",),
+   name="password_change_done",),
 
 ]
--- config/settings.py
+++ config/settings.py
@@ -129,11 +129,9 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+DEFAULT_FROM_EMAIL = "noreply@marketplace.local"
 
 
 AUTH_USER_MODEL = "accounts.User"
 LOGIN_REDIRECT_URL = "home"
 #LOGOUT_REDIRECT_URL = "home"
-
-
-
