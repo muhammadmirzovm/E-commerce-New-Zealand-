3f8af573e2d2ae723d14b997363e53bc81a3ed45
mukhammadmirzo <mukhammadmirzo078@gmail.com>
2026-09-10 16:37:41 +0500

    Product list and Product detail

---
+++ catalog/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+from django.views.generic import ListView, DetailView
+from .models import Product
+
+class ProductListView(ListView):
+   model = Product
+   template_name = "catalog/product_list.html"
+   context_object_name = "products"
+   paginate_by = 2
+
+   def get_queryset(self):
+       return (
+           Product.objects.filter(is_active=True).order_by("-created_at"))
+
+
+class ProductDetailView(DetailView):
+   model = Product
+   template_name = "catalog/product_detail.html"
+   context_object_name = "product"
+
+
