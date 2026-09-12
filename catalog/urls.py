43bccc0cd5c3ad40e82705834561bd181a7b5803
mukhammadmirzo <mukhammadmirzo078@gmail.com>
2026-09-12 15:26:45 +0500

    Style berildi

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path("products/", views.ProductListView.as_view(), name="product_list"),
+   path("", views.ProductListView.as_view(), name="product_list"),
    path("products/<slug:slug>/", views.ProductDetailView.as_view(), name="product_detail"),
 ]
 
